a78b47b63de706aa18030973a65286cea8ba02d4
Seokjun Kim <colus001@me.com>
2026-03-25 12:44:19 +0700

    chore: bump version to 0.2.0

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const agent_mod = @import("agent.zig");
 const init_mod = @import("init.zig");
 const config_editor = @import("config_editor.zig");
 
-const VERSION = "0.1.1";
+const VERSION = "0.2.0";
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
